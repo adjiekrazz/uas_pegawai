73c6aa0148e8b93393f0c16e431946d117d1a73b
adjiekrazz <tibiame.cheatplatinum@gmail.com>
2020-12-05 19:11:19 +0700

    move css assets

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistem Informasi Pegawai</title>
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/custom.css">
 </head>
 <body>
         <?php
--- login/index.php
+++ login/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman Login</title>
     <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../index.css">
+    <link rel="stylesheet" href="../assets/css/custom.css">
 </head>
 <body>
     <?php
